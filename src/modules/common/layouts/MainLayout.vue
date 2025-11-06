59553922aeb00cd3f21917d1fd7b9f39b3c64b8c
samuhlo <samu13lop@gmail.com>
2025-11-06 15:57:38 +0100

    project structure update

--- src/modules/common/layouts/MainLayout.vue
+++ src/modules/common/layouts/MainLayout.vue
@@ -1,46 +1,14 @@
 <template>
-  <div>
-
-    <!-- Mostrar información de carga -->
-    <div v-if="isLoading">
-      <p>Cargando ubicación...</p>
-    </div>
-
-    <!-- Mostrar ubicación cuando esté lista -->
-    <div v-else-if="userLocation">
-      <p class="text-2xl">Tu ubicación actual:</p>
-      <p class="font-bold">Latitud: {{ userLocation[0] }}</p>
-      <p class="font-bold">Longitud: {{ userLocation[1] }}</p>
-    </div>
-
-    <div v-else>
-      <p>No se pudo obtener la ubicación</p>
-    </div>
-  </div>
+    <MapView />
+<div class=" right-0 bottom-0 fixed mb-4 mr-4 logo">
+  <img :src="vueLogo" alt="Vue Logo" class="h-16" />
+</div>
 </template>
 
 <script setup lang="ts">
-import { onMounted } from 'vue'
-import { storeToRefs } from 'pinia'
-import { useMapStore } from '../../map/stores/map.store'
 
+import vueLogo from '../../../assets/vue.svg'
+import MapView from '../../map/views/MapView.vue';
 
-// 1. Obtener el store
-const mapStore = useMapStore()
-
-// 2. Extraer las propiedades REACTIVAS del store (estado y getters)
-const { isLoading, userLocation, isUserLocationReady } = storeToRefs(mapStore)
-
-// 3. Extraer las acciones (funciones) directamente del store
-const { getCurrentPosition } = mapStore
-
-// 4. Cuando el componente se monte, obtener la ubicación
-onMounted(async () => {
-  try {
-    const location = await getCurrentPosition()
-    console.log('Ubicación obtenida:', location)
-  } catch (error) {
-    console.error('Error al obtener ubicación:', error)
-  }
-})
 </script>
+ 
