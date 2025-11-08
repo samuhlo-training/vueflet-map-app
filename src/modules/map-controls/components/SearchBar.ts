e737e6ee816b2d647c994d1967604d1aed4a677b
samuhlo <samu13lop@gmail.com>
2025-11-08 21:30:56 +0100

    update search results ( queda para mañana meterlos en la store de places y hacer los composables )

--- src/modules/map-controls/components/SearchBar.ts
+++ src/modules/map-controls/components/SearchBar.ts
@@ -1,9 +1,11 @@
 import { ref } from "vue";
 import { searchResults } from "./SearchResults";
+import type { Place } from "@/modules/map/interfaces/place.interfaces";
 
 export const useSearchBar = () => {
   const debounceTimer = ref<number | undefined>(undefined);
   const searchQuery = ref("");
+  const searchResults = ref<Place[]>([]);
 
   /**
    * Maneja el evento de input con debounce
@@ -23,9 +25,25 @@ export const useSearchBar = () => {
     debounceTimer.value = window.setTimeout(() => {
       // Paso 3: Ejecutar la búsqueda después de 500ms de inactividad
       console.log("Ejecutando búsqueda con:", searchQuery.value);
-      searchResults();
+      searchInput();
       debounceTimer.value = undefined;
     }, 500); // 500ms de espera
+  };
+
+  const searchInput = async () => {
+    console.log("Buscando ubicación para:", searchQuery.value);
+    // Lógica para buscar la ubicación
+    try {
+      const response = await fetch(
+        `https://nominatim.openstreetmap.org/search?` +
+          `format=json&q=${encodeURIComponent(searchQuery.value)}&limit=5`
+      );
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.error("Error al buscar la ubicación:", error);
+      throw new Error("Error al buscar la ubicación");
+    }
   };
 
   return {
---
+++ src/modules/map/interfaces/place.interfaces.ts
@@ -0,0 +1,16 @@
+export interface Place {
+  addresstype: string;
+  boundingbox: [string, string, string, string]; // [lat_min, lat_max, lon_min, lon_max]
+  class: string;
+  display_name: string;
+  importance: number;
+  lat: string; // Nota: La API lo devuelve como string
+  licence: string;
+  lon: string; // Nota: La API lo devuelve como string
+  name: string;
+  osm_id: number;
+  osm_type: string;
+  place_id: number;
+  place_rank: number;
+  type: string;
+}
